5f91514b7184aa2c4e25f21d37387eece3389b4b
rayhanadri <rayerzz99@gmail.com>
2024-11-18 19:53:26 +0700

    enhancement using map loop to call Square component in board

--- src/App.jsx
+++ src/App.jsx
@@ -31,15 +31,13 @@ function Board({ xIsNext, squares, onPlay }) {
     <>
       <div className="status">{status}</div>
       <div className="board">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        {squares.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
       </div>
     </>
   );
@@ -113,10 +111,10 @@ function calculateWinner(squares) {
     }
   }
 
-  const noNulls  = squares.every(item => item !== null);
-  if(noNulls){
+  const noNulls = squares.every((item) => item !== null);
+  if (noNulls) {
     return "DRAW";
   }
-  
+
   return false;
 }
